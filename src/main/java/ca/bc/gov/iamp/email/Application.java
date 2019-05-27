e1ae8bc6707376c9e9b7176f06bb272c401ea1cc
Emanuel Sousa <emanuel.sousa@opticca.com>
2019-05-27 14:03:06 -0400

    [BCP-393] Initial project setup from existing common components

---
+++ src/main/java/ca/bc/gov/iamp/email/Application.java
@@ -0,0 +1,15 @@
+package ca.bc.gov.iamp.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "ca.bc.gov.iamp")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
